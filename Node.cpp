98d57f770fad0cdc81eb5046eb6f6828a986e24b
EylonK14 <eylony1403@gmail.com>
2024-06-18 00:35:08 +0300

    Fixed Node class

--- Node.cpp
+++ Node.cpp
@@ -1,1 +1,53 @@
 #include "Node.hpp"
+
+
+template<typename T> Node<T>::Node(T data) : _data(data) 
+{
+
+}
+
+template<typename T> Node<T>::~Node()
+{
+    for (auto child : _children)
+    {
+        delete child;
+    }
+}
+
+template<typename T> void Node<T>::addChild(Node<T> child)
+{
+    _children.push_back(child);
+}
+
+template<typename T> void Node<T>::removeChild(Node<T> child)
+{
+    for (size_t i = 0; i < _children.size(); i++)
+    {
+        if (_children[i] == child)
+        {
+            long int index = static_cast<long int>(i);
+            _children.erase(_children.begin() + index);
+            return;
+        }
+    }
+}
+
+template<typename T> T Node<T>::getData()
+{
+    return _data;
+}
+
+template<typename T> Node<T> Node<T>::getChild(int index)
+{
+    if (index < 0 || index >= _children.size())
+    {
+        throw std::out_of_range("Index out of range");
+    }
+    size_t i = static_cast<size_t>(index);
+    return _children[i];
+}
+
+template<typename T> std::vector<Node<T>> Node<T>::getChildren()
+{
+    return _children;
+}
--- Node.hpp
+++ Node.hpp
@@ -1,51 +1,23 @@
 // eylony1403@gmail.com
 
+#pragma once
+
 #include <iostream>
 #include <vector>
 
 template <typename T> 
-
-class Node {
+class Node{
 
     public:
-        Node(T data) : _data(data) {}
-        ~Node() {
-            for (auto child : _children) {
-                delete child;
-            }
-        }
-
-        void addChild(Node<T>* child) {
-            _children.push_back(child);
-        }
-
-        void removeChild(Node<T>* child) {
-            for (size_t i = 0; i < _children.size(); i++) {
-                if (_children[i] == child) {
-                    long int index = static_cast<long int>(i);
-                    _children.erase(_children.begin() + index);
-                    return;
-                }
-            }
-        }
-
-        T getData() {
-            return _data;
-        }
-
-        Node<T>* getChild(int index) {
-            if (index < 0 || index >= _children.size()) {
-                throw std::out_of_range("Index out of range");
-            }
-            size_t i = static_cast<size_t>(index);
-            return _children[i];
-        }
-
-        std::vector<Node<T>*> getChildren() {
-            return _children;
-        }
+        Node(T){};
+        ~Node() {};
+        void addChild(Node<T>) {};
+        void removeChild(Node<T>) {};
+        T getData() {};
+        Node<T> getChild(int) {}
+        std::vector<Node<T>> getChildren() {}
     
     private:
         T _data;
-        std::vector<Node<T>*> _children;
+        std::vector<Node<T>> _children;
 };
